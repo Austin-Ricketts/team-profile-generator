4e1fedebe50989d3536d9c31b7f2f18dfe57a47e
Austin Ricketts <austinricketts4@gmail.com>
2020-05-02 23:46:02 -0400

    added classes, beginning of inquirer prompts

--- app.js
+++ app.js
@@ -10,6 +10,100 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Put a link for your badge here."
+    },
+    {
+        type: "input",
+        name: "repoLink",
+        message: "Please add a link to your repository here."
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please add a note about how to install your application."
+    },
+    {
+        type: "input",
+        name: "usageName1",
+        message: "Please write the name of a dependency here."
+    },
+    {
+        type: "input",
+        name: "usageLink1",
+        message: "Please link to that dependency here."
+    },
+    {
+        type: "input",
+        name: "usageName2",
+        message: "Please write the name of another dependency here."
+    },
+    {
+        type: "input",
+        name: "usageLink2",
+        message: "Please write a link to that dependency here."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please enter a link for your license badge here."
+    },
+    {
+        type: "input",
+        name: "licenseInfo",
+        message: "Please place a link to information regarding your license here."
+    },
+    {
+        type: "input",
+        name: "copyright",
+        message: "What year are you copyrighting this?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please write information about tests here."
+    },
+    {
+        type: "input",
+        name: "developer",
+        message: "What is your GitHub user name?"
+    },
+    {
+        type: "input",
+        name: "profile",
+        message: "Please provide a link to your GitHub profile."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+]).then(function(employees) {
+    const stringToWrite = render(employees);
+
+  fs.writeFile(outputPath, stringToWrite, function(err) {
+
+    if (err) {
+      return console.log(err);
+    }
+    
+
+  });
+});
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
